b10316fec22900520265faa5ff95ac088fbe6f3e
zobayerdev <zobayer.dev@gmail.com>
2024-04-22 17:29:17 +0600

    project upload

---
+++ android/app/src/main/kotlin/com/example/flutter_flight_ui/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_flight_ui
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter_flight_ui/home_screen.dart';
+
+void main() => runApp(
+      DevicePreview(
+        enabled: true,
+        tools: const [
+          ...DevicePreview.defaultTools,
+        ],
+        builder: (context) => const MyApp(),
+      ),
+    );
+
+// void main() {
+//   runApp(const MyApp());
+// }
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Flight App UI',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
